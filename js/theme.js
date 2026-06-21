f7c8c5f86feb11c1f9ee907f90a4b3eb10442350
Saadi-Rahman <sadi6280@gmail.com>
2026-06-22 03:59:52 +0600

    added dark-light theme.

---
+++ js/theme.js
@@ -0,0 +1,38 @@
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = document.getElementById("themeIcon");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "light") {
+  document.documentElement.classList.remove("dark");
+
+  if (themeIcon) {
+    themeIcon.className = "fa-regular fa-sun";
+  }
+} else {
+  document.documentElement.classList.add("dark");
+
+  if (themeIcon) {
+    themeIcon.className = "fa-solid fa-moon";
+  }
+}
+
+// Toggle theme
+if (themeToggle) {
+  themeToggle.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+
+      if (themeIcon) {
+        themeIcon.className = "fa-solid fa-moon";
+      }
+    } else {
+      localStorage.setItem("theme", "light");
+
+      if (themeIcon) {
+        themeIcon.className = "fa-regular fa-sun";
+      }
+    }
+  });
+}
